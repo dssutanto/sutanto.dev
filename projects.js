f5e2de0e9874f4761a664db315bd9b79e6340834
dssutanto <derrick_sutanto@msn.com>
2021-10-30 04:04:33 -0700

    Added public-cjur

---
+++ projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+    {
+        url: "assets/laptop/scene.gltf",
+        name: "cms",
+        title: "CJUR",
+        desc: "Full-stack solution",
+        model: null,
+        shape: null,
+        page: "",
+        link: "https://cjur.sutanto.dev"
+    },
+    {
+        url: "assets/pokeball/scene.gltf",
+        name: "pokedex",
+        title: "Pokédex",
+        desc: "Database project",
+        model: null,
+        shape: null,
+        page: "",
+        link: "https://github.com/dssutanto/cs304pokedex"
+    },
+    {
+        url: "assets/flask/scene.gltf",
+        name: "cjur",
+        title: "CJUR",
+        desc: "Website front end",
+        model: null,
+        shape: null,
+        page: "./CJUR.html",
+        link: "https://cjur.ca/"
+    },    
+];
---
+++ resume.js
@@ -0,0 +1,5 @@
+export const resume = [
+    "Canadian Journal of Undergraduate Research (10/19-8/21)",
+    "UBC Dept. of Sociology (9/20 - 4/21)",
+    "UBC Dept. of Physical Therapy (9/18 - 4/19)"
+];
